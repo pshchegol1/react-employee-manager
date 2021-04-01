de929dd0e20cbe97d1b5d573697dc4a28e5bd895
pshchegol1 <46579720+pshchegol1@users.noreply.github.com>
2021-04-01 14:32:39 -0600

    changed the nav bar

--- src/components/appbar/AppBar.jsx
+++ src/components/appbar/AppBar.jsx
@@ -3,18 +3,34 @@ import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 
 const AppBarStyles = styled.nav `
-    box-shadow: 0 0 3px 1px grey;
-    padding: 1rem;
+box-shadow: 0 0 3px 1px grey;
+  
 
-    ul
-    {
-        display:flex;
-        justify-content: center;
-    }
-    li
-    {
-        margin: 0 1rem 0;
-    }
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #4169E1;
+    display: flex;
+    justify-content:center;
+  }
+  
+
+  
+  li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+    font-weight: bold;
+  }
+  
+  /* Change the link color to #00BFFF (DeepSkyBlue) on hover */
+  li a:hover {
+    background-color: #00BFFF;
+  }
   
 
 
